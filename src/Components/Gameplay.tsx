e66e80cce41520f38117305da27ec8a71842c69f
Libre255 <briangbv@outlook.com>
2021-04-09 10:55:37 +0200

    feature/6-save-numbers-once-it-hits-top

--- src/Components/Gameplay.tsx
+++ src/Components/Gameplay.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ControlBoxesGrid from "./ControlBoxesGrid";
 import useControlNr from "../Hooks/useControlNr";
 import useControlMovements from "../Hooks/useControlMovements";
+import OneTopBox from "./OneTopBox";
 
 const Gameplay: React.FC = () => {
-  const { selectedNr, randomNr, boxToTop } = useControlNr();
   const { bottomBoxPosition, shotAnimation } = useControlMovements();
+  const { selectedNr, randomNr, boxToTop, TopBoxesArray } = useControlNr(
+    bottomBoxPosition
+  );
 
   return (
     <div id="GamePlayBox" className="testBox2 ">
+      {TopBoxesArray.map((TopBox, index) => (
+        <OneTopBox key={index} NrValue={TopBox} />
+      ))}
       <div className="testBox GlobalStyleNrs" style={shotAnimation}>
         {boxToTop}
       </div>
--- src/Hooks/useControlNr.ts
+++ src/Hooks/useControlNr.ts
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import useKeyPressed from "./useKeyPressed";
 import shuffleArray from "../Methods/shuffleArray";
+
 const upComingBoxes: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //This can be remplaced with fetch from database
 
-const useControlNr = () => {
+const useControlNr = (position?: number) => {
   const [randomNr, setRandomNr] = useState<number>(
     upComingBoxes[Math.floor(Math.random() * upComingBoxes.length)]
   );
-  const [boxToTop, setBoxToTop] = useState<number | undefined>(undefined);
   const [selectedNr, setSelectedNr] = useState<number | undefined>(undefined);
+  const [TopBoxesArray, setTopBoxesArray] = useState<
+    [number, number, number, number, number]
+  >([0, 0, 0, 0, 0]);
+  const [boxToTop, setBoxToTop] = useState<number>();
 
   const z_Key_Pressed = useKeyPressed("z");
   const x_Key_Pressed = useKeyPressed("x");
@@ -31,10 +35,17 @@ const useControlNr = () => {
       setSelectedNr(randomNr);
     } else if (x_Key_Pressed) {
       setBoxToTop(selectedNr);
+      setTopBoxesArray((pv) => {
+        if (selectedNr === undefined) {
+          return pv;
+        }
+        pv[position! - 1] = selectedNr;
+        return pv;
+      });
     }
   }, [z_Key_Pressed, x_Key_Pressed]);
 
-  return { randomNr, selectedNr, boxToTop };
+  return { randomNr, selectedNr, boxToTop, TopBoxesArray };
 };
 
 export default useControlNr;
