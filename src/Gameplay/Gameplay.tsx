c71ed0f73ee9b73620414372707e828581a9d119
Libre255 <briangbv@outlook.com>
2021-08-03 21:39:28 +0200

    added display for amounttimes added

--- src/Gameplay/Gameplay.tsx
+++ src/Gameplay/Gameplay.tsx
@@ -19,7 +19,7 @@ const Gameplay: React.FC = () => {
       {containerOfRows.map((row, index) => (
         <div key={index} className="RowStyle" style={{ gridRow: index + 1 }}>
           {row.map((box, index) => (
-            <Box key={index} boxValue={box.value} />
+            <Box key={index} boxValue={box.value} amountTimesAdded={box.AmountTimesAdded} />
           ))}
         </div>
       ))}
--- src/Gameplay/gamePlayManagement/components/Box.tsx
+++ src/Gameplay/gamePlayManagement/components/Box.tsx
@@ -2,9 +2,15 @@ import React from "react";
 
 interface props {
   boxValue: number;
+  amountTimesAdded:number;
 }
-const Box: React.FC<props> = ({ boxValue }: props) => (
-  <div className="Box testBox GlobalStyleNrs">{boxValue}</div>
+const Box: React.FC<props> = ({ boxValue, amountTimesAdded }: props) => (
+  <div className="Box testBox GlobalStyleNrs">
+    <div className="amountTimesAdded-Box">
+      {amountTimesAdded}
+    </div>
+    {boxValue}
+  </div>
 );
 
 export default Box;
