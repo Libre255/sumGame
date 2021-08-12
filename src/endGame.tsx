dc830904a84f788049316591ecd74cde48f14548
Libre255 <briangbv@outlook.com>
2021-08-12 14:05:36 +0200

    finished

--- src/endGame.tsx
+++ src/endGame.tsx
@@ -4,7 +4,10 @@ interface Props {
   total: number;
 }
 const EndGame: React.FC<Props> = ({ total }) => {
-  return <div id="endGameBox">Your total Sum score is {total}!</div>;
+  return <div id="endGameBox">
+    <img src="https://media.giphy.com/media/xnowvAEfh0WFRhqJqM/giphy.gif" alt="game over img" />
+    
+    Your total Sum score is {total}!</div>;
 };
 
 export default EndGame;
